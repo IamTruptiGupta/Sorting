7eeb4cd0597606de957b20c21ec14e8ffaaa08c2
Trupti <Trupti@10.12.97.24>
2024-02-08 10:31:16 +0530

    Bubble Sort

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,17 @@
-/**
- * 
- */
-/**
- * 
- */
-module Sorting {
+package Day1;
+
+import java.unil.Scanner;
+public class Reverse {
+	public static void main (String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number: ")
+		int number = scanner.nextint();
+		int reversedNumber=0, remainder;
+		while (number !=0) {
+			remainder = number %10;
+			reversedNumber = reversedNumber * 10 + remainder;
+			number /=10;
+		}
+		System.out.printIn("Reversed number: "+reversedNumber);
+	}
 }
